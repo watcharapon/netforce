876485c5be140168194e25fb76d95eeef795cabb
David Janssens <dj@netforce.com>
2015-10-07 19:02:27 +0700

    move support issues to other module

---
+++ netforce_support/netforce_support/models/issue.py
@@ -0,0 +1,57 @@
+from netforce.model import Model,fields,get_model
+from netforce import access
+from datetime import *
+import time
+
+class Issue(Model):
+    _name="issue"
+    _string="Issue"
+    _fields={
+        "number": fields.Char("Number",required=True,search=True),
+        "date_created": fields.DateTime("Date Created",required=True,search=True),
+        "date_closed": fields.DateTime("Date Closed"),
+        "contact_id": fields.Many2One("contact","Customer",required=True,search=True),
+        "project_id": fields.Many2One("project","Project",required=True,search=True),
+        "description": fields.Text("Description",required=True,search=True),
+        "priority": fields.Char("Priority",required=True),
+        "emails": fields.One2Many("email.message", "related_id", "Emails"),
+        "state": fields.Selection([["open","Open"],["in_progress","In Progress"],["closed","Closed"],["wait_customer","Wait For Customer"],["wait_internal","Internal Wait"]],"Status",required=True,search=True),
+        "days_open": fields.Integer("Days Open",function="get_days_open"),
+        "resource_id": fields.Many2One("service.resource","Assigned To"),
+        "documents": fields.One2Many("document", "related_id", "Documents"),
+        "emails": fields.One2Many("email.message", "related_id", "Emails"),
+    }
+    _order="priority,id"
+
+    def _get_number(self, context={}):
+        seq_id = get_model("sequence").find_sequence(type="issue")
+        if not seq_id:
+            return None
+        while 1:
+            num = get_model("sequence").get_next_number(seq_id)
+            user_id = access.get_active_user()
+            access.set_active_user(1)
+            res = self.search([["number", "=", num]])
+            access.set_active_user(user_id)
+            if not res:
+                return num
+            get_model("sequence").increment_number(seq_id)
+
+    _defaults={
+        "date_created": lambda *a: time.strftime("%Y-%m-%d %H:%M:%S"),
+        "state": "open",
+        "number": _get_number,
+    }
+
+    def get_days_open(self,ids,context={}):
+        vals={}
+        today=date.today()
+        for obj in self.browse(ids):
+            if obj.state=="closed":
+                vals[obj.id]=None
+                continue
+            d=datetime.strptime(obj.date_created,"%Y-%m-%d %H:%M:%S").date()
+            vals[obj.id]=(today-d).days
+        return vals
+
+Issue.register()
