a7977c233cf2d407ffc92a0a054aa3bb787aa6b0
David Janssens <dj@netforce.com>
2015-11-02 16:58:13 +0700

    improve service dashboard

--- netforce_service/netforce_service/models/report_service_time.py
+++ netforce_service/netforce_service/models/report_service_time.py
@@ -37,12 +37,15 @@ class ReportServiceTime(Model):
                 "weeks": {},
             }
         res = db.query(
-            "SELECT project_id,week,SUM(actual_hours) AS total_hours FROM work_time WHERE state='approved' GROUP BY project_id,week")
+            "SELECT project_id,week,SUM(actual_hours) AS total_actual_hours,SUM(bill_hours) AS total_bill_hours FROM work_time WHERE state='approved' GROUP BY project_id,week")
         for r in res:
             proj = projects.get(r.project_id)
             if not proj:
                 continue
-            proj["weeks"][r.week] = r.total_hours
+            proj["weeks"][r.week] = {
+                "actual_hours": r.total_actual_hours,
+                "bill_hours": r.total_bill_hours,
+            }
         weeks = []
         d = datetime.today()
         d -= timedelta(d.weekday())
@@ -57,16 +60,22 @@ class ReportServiceTime(Model):
             line = {
                 "project_name": proj["name"],
                 "weeks": [],
-                "older": 0,
-                "total": 0,
+                "older": {
+                    "actual_hours": 0,
+                    "bill_hours": 0,
+                },
+                "total_actual_hours": 0,
+                "total_bill_hours": 0,
             }
             for w in weeks:
-                n = proj["weeks"].get(w, 0)
-                line["weeks"].append(n)
-            for w, n in proj["weeks"].items():
-                line["total"] += n
+                vals = proj["weeks"].get(w, {"actual_hours":0,"bill_hours":0})
+                line["weeks"].append(vals)
+            for w, vals in proj["weeks"].items():
+                line["total_actual_hours"] += vals["actual_hours"]
+                line["total_bill_hours"] += vals["bill_hours"]
                 if w < weeks[-1]:
-                    line["older"] += n
+                    line["older"]["actual_hours"] += vals["actual_hours"]
+                    line["older"]["bill_hours"] += vals["bill_hours"]
             lines.append(line)
         data = {
             "lines": lines,
@@ -84,12 +93,15 @@ class ReportServiceTime(Model):
                 "weeks": {},
             }
         res = db.query(
-            "SELECT resource_id,week,SUM(actual_hours) AS total_hours FROM work_time WHERE state='approved' GROUP BY resource_id,week")
+            "SELECT resource_id,week,SUM(actual_hours) AS total_actual_hours,SUM(bill_hours) AS total_bill_hours FROM work_time WHERE state='approved' GROUP BY resource_id,week")
         for r in res:
             resource = resources.get(r.resource_id)
             if not resource:
                 continue
-            resource["weeks"][r.week] = r.total_hours or 0
+            resource["weeks"][r.week] = {
+                "actual_hours": r.total_actual_hours or 0,
+                "bill_hours": r.total_bill_hours or 0,
+            }
         weeks = []
         d = datetime.today()
         d -= timedelta(d.weekday())
@@ -104,17 +116,24 @@ class ReportServiceTime(Model):
             line = {
                 "resource_name": resource["name"],
                 "weeks": [],
-                "older": 0,
-                "total": 0,
+                "older": {
+                    "actual_hours": 0,
+                    "bill_hours": 0,
+                },
+                "total_actual_hours": 0,
+                "total_bill_hours": 0,
             }
             for w in weeks:
-                n = resource["weeks"].get(w, 0)
-                line["weeks"].append(n)
-            for w, n in resource["weeks"].items():
-                line["total"] += n
+                vals = resource["weeks"].get(w, {"actual_hours":0,"bill_hours":0})
+                line["weeks"].append(vals)
+            for w, vals in resource["weeks"].items():
+                line["total_actual_hours"] += vals["actual_hours"]
+                line["total_bill_hours"] += vals["bill_hours"]
                 if w < weeks[-1]:
-                    line["older"] += n
+                    line["older"]["actual_hours"]+=vals["actual_hours"]
+                    line["older"]["bill_hours"]+=vals["bill_hours"]
             lines.append(line)
+        lines.sort(key=lambda l: l["resource_name"])
         data = {
             "lines": lines,
         }
