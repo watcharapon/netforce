cf1c299ea1480390bae000b930f88a6417d7cf1c
David Janssens <dj@netforce.com>
2015-11-03 19:40:22 +0700

    xxx

--- netforce_delivery/netforce_delivery/models/delivery_route.py
+++ netforce_delivery/netforce_delivery/models/delivery_route.py
@@ -9,7 +9,7 @@ class Route(Model):
     _fields={
         "number": fields.Char("Number",required=True,search=True),
         "date": fields.Date("Date",required=True,search=True),
-        "carrier_id": fields.Many2One("delivery.carrier","Carrier",required=True,search=True),
+        "carrier_id": fields.Many2One("delivery.carrier","Carrier",search=True),
         "lines": fields.One2Many("delivery.route.line","route_id","Destinations"),
         "state": fields.Selection([["planned","Planned"],["transit","In Transit"],["done","Completed"],["canceled","Canceled"]],"Status",required=True),
         "num_lines": fields.Integer("# Items",function="get_num_lines"),
