e3e4127b95b86fa794fd8f1cdd3cbbd0c7c296c7
David Janssens <dj@netforce.com>
2015-11-19 12:59:17 +0700

    missing file

---
+++ netforce_support/netforce_support/models/message.py
@@ -0,0 +1,69 @@
+# Copyright (c) 2012-2015 Netforce Co. Ltd.
+# 
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+# IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+# DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+# OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE
+# OR OTHER DEALINGS IN THE SOFTWARE.
+
+from netforce.model import Model, fields, get_model
+import json
+
+class Message(Model):
+    _inherit = "message"
+
+    def create(self,vals,*args,**kw):
+        new_id=super().create(vals,*args,**kw)
+        obj=self.browse(new_id)
+        if obj.related_id._model=="issue":
+            issue=obj.related_id
+            project=issue.project_id
+            contact=issue.contact_id
+            emails=issue.get_email_addresses()
+            if emails:
+                vals={
+                    "from_addr": "support@netforce.com", # XXX
+                    "to_addrs": ",".join(emails),
+                    "subject": "New message for issue %s: %s"%(issue.number,obj.subject),
+                    "body": obj.body,
+                    "state": "to_send",
+                    "name_id": "contact,%s"%contact.id,
+                    "related_id": "issue,%s"%issue.id,
+                }
+                get_model("email.message").create(vals)
+        return new_id
+
+    def write(self,ids,vals,*args,**kw):
+        super().write(ids,vals,*args,**kw)
+        for obj in self.browse(ids):
+            if obj.related_id._model=="issue":
+                issue=obj.related_id
+                project=issue.project_id
+                contact=project.contact_id
+                emails=issue.get_email_addresses()
+                if emails:
+                    body=json.dumps(vals)
+                    vals={
+                        "from_addr": "support@netforce.com", # XXX
+                        "to_addrs": ",".join(emails),
+                        "subject": "Message modified for issue %s: %s"%(issue.number,obj.subject),
+                        "body": body,
+                        "state": "to_send",
+                        "name_id": "contact,%s"%contact.id,
+                        "related_id": "issue,%s"%issue.id,
+                    }
+                    get_model("email.message").create(vals)
+
+Message.register()
