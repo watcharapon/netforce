01c185c08028f117eef585fd6d7963a3265dedf9
David Janssens <dj@netforce.com>
2015-11-20 17:30:11 +0700

    fix invoice onchange_sequence date

--- netforce_service/netforce_service/models/work_time.py
+++ netforce_service/netforce_service/models/work_time.py
@@ -29,6 +29,7 @@ class WorkTime(Model):
     _string = "Work Time"
     _fields = {
         "resource_id": fields.Many2One("service.resource", "Resource", required=True, search=True, on_delete="cascade"),
+        "resource_type": fields.Selection([["person","Person"],["machine","Machine"]],"Resource Type",function="_get_related",function_search="_search_related",function_context={"path":"resource_id.type"},search=True),
         "project_id": fields.Many2One("project", "Project", search=True, required=True),
         "related_id": fields.Reference([["job","Service Order"],["sale.order","Sales Order"],["rental.order","Rental Order"]],"Related To",search=True),
         "job_id": fields.Many2One("job", "Service Order", search=True), # XXX: deprecated
