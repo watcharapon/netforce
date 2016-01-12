31e1e1680fc84e0bf669391267248c9d05ac0dcd
SPP <songpon.p@gmail.com>
2016-01-12 12:25:51 +0700

    can not show po line (issue #9)

--- netforce_purchase/netforce_purchase/models/purchase_order_line.py
+++ netforce_purchase/netforce_purchase/models/purchase_order_line.py
@@ -121,7 +121,7 @@ class PurchaseOrderLine(Model):
                 for line in inv.lines:
                     prod_id = line.product_id.id
                     inv_qtys.setdefault(prod_id, 0)
-                    inv_qtys[prod_id] += line.qty 
+                    inv_qtys[prod_id] += line.qty or 0
             for line in order.lines:
                 if line.id not in ids:
                     continue
