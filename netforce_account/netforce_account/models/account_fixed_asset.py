0cc532d602f5420a9cec8b5340102e4911bf68ce
watcha.h <watcha.h@almacom.co.th>
2016-03-25 16:16:04 +0700

    Asset Number should be correspond Purchase Date ref: http://dev.netforce.com/online/helpdesk-support/issues/164

--- netforce_account/netforce_account/models/account_fixed_asset.py
+++ netforce_account/netforce_account/models/account_fixed_asset.py
@@ -63,11 +63,11 @@ class FixedAsset(Model):
         if not seq_id:
             return None
         while 1:
-            num = get_model("sequence").get_next_number(seq_id)
+            num = get_model("sequence").get_next_number(seq_id,context)
             res = self.search([["number", "=", num]])
             if not res:
                 return num
-            get_model("sequence").increment_number(seq_id)
+            get_model("sequence").increment_number(seq_id,context)
 
     _defaults = {
         "state": "pending",
