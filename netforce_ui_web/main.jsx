3b6471353779580c3fe485ac417e218acc0ccb81
David Janssens <dj@netforce.com>
2016-04-04 10:26:20 +0700

    web ui

---
+++ netforce_ui_web/main.jsx
@@ -0,0 +1,6 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+require("bootstrap/dist/css/bootstrap.css");
+
+ReactDOM.render(<p>Hello world!</p>,document.getElementById("content"));
