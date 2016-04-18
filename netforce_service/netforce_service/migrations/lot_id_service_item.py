03217b9781d5b841a2e1dc1d32348936e3246eb3
Bank <thanachai.w@almacom.co.th>
2016-04-18 13:58:12 +0700

    create new lot for the case that there is no existing lot no

--- netforce_service/netforce_service/migrations/lot_id_service_item.py
+++ netforce_service/netforce_service/migrations/lot_id_service_item.py
@@ -8,9 +8,14 @@ class Migration(migration.Migration):
 
     def migrate(self):
         for obj in get_model("service.item").search_browse([[]]):
-            lot = get_model("stock.lot").search_browse([["number","=",obj.serial_no]])
-            if len(lot)>0:
-                print(obj.number,obj.serial_no,lot[0].number,lot[0].id)
-                obj.write({"lot_id": lot[0].id})
+            if obj.serial_no:
+                lot = get_model("stock.lot").search_browse([["number","=",obj.serial_no]])
+                if len(lot)>0:
+                    print("Lot Number %s is already exist"%obj.number)
+                    obj.write({"lot_id": lot[0].id})
+                else:
+                    print("Create Lot for %s which serial no is %s"%(obj.number,obj.serial_no))
+                    lot_id = get_model("stock.lot").create({"number": obj.serial_no})
+                    obj.write({"lot_id": lot_id})
 
 Migration.register()
