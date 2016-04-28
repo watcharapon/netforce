6bc650f707719793356ca19e5ff7d4e09b4a34d3
David Janssens <dj@netforce.com>
2016-04-28 16:11:40 +0700

    fix

--- netforce/netforce/job.py
+++ netforce/netforce/job.py
@@ -91,6 +91,7 @@ def start():
     global _check_times
     print("Running jobs in process %s..."%os.getpid())
     dbname = config.get("database")
+    schema = config.get("schema")
     if dbname:
         check_dbs = [dbname]
     else:
@@ -102,7 +103,7 @@ def start():
     _check_times = manager.dict({db: t for db in check_dbs})
     for dbname in check_dbs:
         print("resetting jobs of db '%s'"%dbname)
-        db=database.connect(dbname)
+        db=database.connect(dbname,schema)
         res=db.execute("UPDATE cron_job SET state='waiting' WHERE state in ('running','error')")
         db.commit()
     job_pool = Pool(processes=int(config.get("job_processes")))
@@ -116,7 +117,7 @@ def start():
                     continue
                 _check_times[dbname] = t0 + timedelta(seconds=60)
                 print("Checking for scheduled jobs in database %s..." % dbname)
-                db = database.connect(dbname)
+                db = database.connect(dbname,schema)
                 db.begin()
                 res = db.query("SELECT * FROM cron_job WHERE state='waiting' ORDER BY date")
                 db.commit()
