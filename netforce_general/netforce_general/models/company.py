64a23544fd0b4c73c015b706dade8b1f7d5a58b2
watcha.h <watcha.h@almacom.co.th>
2016-07-22 14:10:03 +0700

    company should use code as key to import/export Note: our system not support to import name with comma inside text ex "B Click Auto Co.,Ltd.,"

--- netforce_general/netforce_general/models/company.py
+++ netforce_general/netforce_general/models/company.py
@@ -29,6 +29,7 @@ class Company(Model):
     _string = "Company"
     _key = ["name"]
     _audit_log = True
+    _export_field="code"
     _fields = {
         "name": fields.Char("Company Name", required=True, search=True, translate=True),
         "code": fields.Char("Company Code", search=True),
