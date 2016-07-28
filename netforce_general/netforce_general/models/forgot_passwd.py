95d6a2f69db485fb87b0e0925bf930fbfb4c6a0a
anas.t@almacom.co.th <anas.t@almacom.co.th>
2016-07-28 17:04:40 +0700

    Remove new function forgot password in general module ** If you want to use it, you can use netforce_base_general module in git lab ref : http://dev.netforce.com/online/helpdesk-support/issues/368

--- netforce_general/netforce_general/models/forgot_passwd.py
+++ netforce_general/netforce_general/models/forgot_passwd.py
@@ -19,10 +19,7 @@
 # OR OTHER DEALINGS IN THE SOFTWARE.
 
 from netforce.model import Model, fields, get_model
-from netforce.database import get_connection
-from netforce import config, database
 from random import choice
-import urllib.request as request
 import string
 from netforce import access
 
@@ -33,16 +30,9 @@ class ForgotPasswd(Model):
     _fields = {
         "email": fields.Char("Enter your email:", required=True),
         "key": fields.Char("Key"),
-        "is_reset" : fields.Boolean("Reset"),
-        "show_dbs": fields.Boolean("Show DBs"),
-        "db_name": fields.Selection([], "Database", required=True),
     }
 
     def create(self, vals, **kw):
-        db_name = vals.get("db_name")
-        if not db_name:
-            raise Exception("Missing db name")
-        database.set_active_db(db_name)
         uid = access.get_active_user()
         try:
             access.set_active_user(1)
@@ -50,154 +40,45 @@ class ForgotPasswd(Model):
         finally:
             access.set_active_user(uid)
 
-    def write(self,ids,vals,**kw):
-        if ids:
-            if vals.get("db_name"):
-                database.set_active_db(vals.get("db_name"))
-            else:
-                database.set_active_db(self.browse(ids[0]).db_name)
-            access.set_active_user(1)
-            super().write(ids,vals,**kw)
-
-    def get_databases(self, context={}):
-        if config.get("database"):
-            dbname = config.get("database")
-            return [(dbname, dbname)]
-        elif config.get("database_from_domain"):
-            request_context = context["request"]
-            host = request_context.host
-            subdom = host.split(".", 1)[0]
-            if subdom not in ("all", "clients"):  # XXX
-                dbname = subdom.replace("-", "_")
-                return [(dbname, dbname)]
-        elif config.get("database_from_http_header"):
-            request_context = context["request"]
-            dbname = request_context.headers.get("X-Database")
-            return [(dbname, dbname)]
-        db_list = sorted(database.list_databases())
-        return [(x, x) for x in db_list]
-
     def default_get(self, field_names=None, context={}, **kw):
-        data={}
         chars = string.ascii_letters + string.digits
         length = 8
         key = ''.join([choice(chars) for _ in range(length)])
-        data['key'] = key
-        if context :
-            data['show_dbs'] = get_model("login").get_show_dbs(context=context)
-            data['db_name'] = get_model("login").get_db_name(context=context)
-            if context.get("email"):
-                data['email'] = context.get("email")
-        return data
+        return {
+            "key": key
+        }
 
-    def check_connectivity(self, reference):
-        try:
-            request.urlopen(reference, timeout=1)
-            return True
-        except request.URLError:
-            return False
-
-    def _send_email(self, ids, context={}):
-        db = get_connection()
-        request_context = context.get("request")
-        #for check your internet connection
+    def send_email(self, ids, context={}):
+        uid = access.get_active_user()
         try:
             access.set_active_user(1)
             obj = self.browse(ids[0])
-            db.commit()
             # XXX "=" should be changed to "=ilike" later
             res = get_model("base.user").search((["email", "=ilike", obj.email.strip()]))
             if not res:
                 raise Exception("User with given email doesn't exist in database")
-            '''
-                EX:
-                    mgt.netforce.com/forgot_password?email=anas.t@almacom.co.th&url=1234567
-            '''
-
-            host = request_context.host
-            protocol = request_context.protocol
-
-            key = obj.key
-            action = "change_passwd"
-            #key is always change every time, dont worry for cache
-            url='http://mgt.netforce.com/forgot_password?email=%s&host=%s&action=%s&key=%s&protocol=%s'%(obj.email,host,action,key,protocol)
-            internet = self.check_connectivity('https://www.google.co.th/')
-            if not internet:
-                raise Exception("Please check your internet connection")
-            try:
-                rq=request.urlopen(url)
-                data=rq.read()
-                if data:
-                    data = eval(data)
-                    if data.get("status"):
-                        return {"next": {"name": "forgot_passwd_done","email" : obj.email}}
-                else:
-                    raise Exception("Please contact support@netforce.com")
-            except Exception as e:
-                msg=e
-                print("ERROR ", e)
-                return {
-                    'flash': 'ERROR: %s'%msg,
-                    'type': 'error',
+            request = context["request"]
+            msg = "Somebody requested change of your Netforce password.\n\n"
+            msg += "If you didn't do it, please ignore this message.\n\n"
+            msg += "Otherwise if you want to reset your password click on the link below:\n\n"
+            msg += "http://%s/ui#name=change_passwd&key=%s\n\n\n" % (request.host, obj.key)
+            msg += "Regards,\n"
+            msg += "Netforce Team"
+            vals = {
+                "type": "out",
+                "from_addr": "support@netforce.com",
+                "to_addrs": obj.email,
+                "subject": "Netforce reset password request",
+                "body": msg,
+                "state": "to_send",
+            }
+            get_model("email.message").create(vals)
+            return {
+                "next": {
+                    "name": "forgot_passwd_done"
                 }
-        except Exception as e:
-            import traceback
-            traceback.print_exc()
-            raise Exception(e)
-
-    def send_email(self, context={}):
-        data = context.get("data")
-        db_name = data.get("db_name")
-        if not db_name:
-            raise Exception("Missing db name")
-        database.set_active_db(db_name)
-        email = data.get("email")
-        key = data.get("key")
-        db = get_connection()
-        request_context = context.get("request")
-        #for check your internet connection
-        try:
-            access.set_active_user(1)
-            # XXX "=" should be changed to "=ilike" later
-            res = get_model("base.user").search((["email", "=ilike", email.strip()]))
-            if not res:
-                raise Exception("User with given email doesn't exist in database")
-            '''
-                EX:
-                    mgt.netforce.com/forgot_password?email=anas.t@almacom.co.th&url=1234567
-            '''
-
-            host = request_context.host
-            protocol = request_context.protocol
-
-            action = "change_passwd"
-            #key is always change every time, dont worry for cache
-            url='http://mgt.netforce.com/forgot_password?email=%s&host=%s&action=%s&key=%s&protocol=%s&db_name=%s'%(email,host,action,key,protocol,db_name)
-            internet = self.check_connectivity('https://www.google.co.th/')
-            #create data
-            self.create(data)
-            db.commit()
-            if not internet:
-                raise Exception("Please check your internet connection")
-            try:
-                rq=request.urlopen(url)
-                data=rq.read()
-                if data:
-                    data = eval(data)
-                    if data.get("status"):
-                        return {"next": {"name": "forgot_passwd_done","email" : email}}
-                else:
-                    raise Exception("Please contact support@netforce.com")
-            except Exception as e:
-                msg=e
-                print("ERROR ", e)
-                return {
-                    'flash': 'ERROR: %s'%msg,
-                    'type': 'error',
-                }
-        except Exception as e:
-            import traceback
-            traceback.print_exc()
-            raise Exception(e)
+            }
+        finally:
+            access.set_active_user(uid)
 
 ForgotPasswd.register()
