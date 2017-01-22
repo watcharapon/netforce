5b48ac459166b1a8c2a04f22aabb21c861241c81
Songpon Phusing <songpon.p@almacom.co.th>
2017-01-22 16:11:27 +0700

    quotation set key company+number

--- netforce_sale/netforce_sale/models/sale_quot.py
+++ netforce_sale/netforce_sale/models/sale_quot.py
@@ -1,15 +1,15 @@
 # Copyright (c) 2012-2015 Netforce Co. Ltd.
-# 
+#
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
 # in the Software without restriction, including without limitation the rights
 # to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 # copies of the Software, and to permit persons to whom the Software is
 # furnished to do so, subject to the following conditions:
-# 
+#
 # The above copyright notice and this permission notice shall be included in all
 # copies or substantial portions of the Software.
-# 
+#
 # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
 # EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
 # MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
@@ -32,7 +32,7 @@ class SaleQuot(Model):
     _string = "Quotation"
     _audit_log = True
     _name_field = "number"
-    _key = ["number"]
+    _key = ["number","company_id"]
     _multi_company = True
     _fields = {
         "number": fields.Char("Number", required=True, search=True),
